50f36d11daff13ada19df05a1e5ba922fb3530a5
MAC <mac@MACs-Mac-mini.lan>
2022-01-13 17:35:59 +0700

    fixed scanner repo

--- main.go
+++ main.go
@@ -12,25 +12,46 @@ type users struct {
 	Name  string `json:"name"`
 }
 
+type commit struct {
+	Message string `json:"message"`
+}
+
+type responseCommit struct {
+	Commit commit `json:"commit"`
+}
+
+var baseUrl string = "https://api.github.com"
+var input string
+
 func main() {
 
-	fmt.Println("Hello word")
-	req, err := http.NewRequest(http.MethodGet, "https://api.github.com/user", nil)
+	config := ReadConfig()
+	body := requestApiGithub(config, baseUrl+"/user")
+	user := users{}
+	err := json.Unmarshal(body, &user)
+	PanicIfError(err)
+	fmt.Println("Welcome To Generate Timesheet " + user.Name)
+	PanicIfError(err)
+	fmt.Println("Masukkan repo :")
+	fmt.Scanf("%s", &input)
+	repo := requestApiGithub(config, baseUrl+"/repos/"+user.Login+"/"+input+"/commits")
+	commit := []responseCommit{}
+	err = json.Unmarshal(repo, &commit)
+	PanicIfError(err)
+	fmt.Println(commit)
+}
+
+func requestApiGithub(config Config, url string) []byte {
+	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		panic(err)
 	}
-	req.Header.Add("Authorization", "token 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")
+	req.Header.Add("Authorization", "token "+config.AccessToken)
 	req.Header.Add("Accept", "application/json")
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		panic(err)
 	}
 	body, _ := ioutil.ReadAll(res.Body)
-	user := users{}
-	err = json.Unmarshal(body, &user)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(user)
+	return body
 }
