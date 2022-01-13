74d90205224ea12880eb5a434cbb85a11339ed21
MAC <mac@MACs-Mac-mini.lan>
2022-01-13 17:34:31 +0700

    add config file

---
+++ config.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+type Config struct {
+	Username    string `json:"username"`
+	AccessToken string `json:"access_token"`
+}
+
+func GenerateConfig() {
+	config := Config{
+		Username:    "",
+		AccessToken: "",
+	}
+	jsonConfig, err := json.MarshalIndent(config, "", "")
+	if err != nil {
+		panic(err)
+	}
+	err = ioutil.WriteFile("./config.json", jsonConfig, 0777)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func ReadConfig() Config {
+	config := Config{}
+	file, err := ioutil.ReadFile("./config.json")
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(file, &config)
+	return config
+}
