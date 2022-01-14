18f3258b88c8bd5cbbda184dacb04a75747a2d0b
MAC <mac@MACs-Mac-mini.lan>
2022-01-14 11:24:05 +0700

    add input since date

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 type users struct {
@@ -20,10 +21,17 @@ type responseCommit struct {
 	Commit commit `json:"commit"`
 }
 
-var baseUrl string = "https://api.github.com"
-var input string
+var (
+	baseUrl   string = "https://api.github.com"
+	input     string
+	year, day int
+	month     time.Month
+	resCommit []responseCommit
+)
 
 func main() {
+	// date := "2022-01-12"
+	// parse, _ := time.Parse("2022-01-12T00:00:00Z", date)
 
 	config := ReadConfig()
 	body := requestApiGithub(config, baseUrl+"/user")
@@ -34,11 +42,21 @@ func main() {
 	PanicIfError(err)
 	fmt.Println("Masukkan repo :")
 	fmt.Scanf("%s", &input)
-	repo := requestApiGithub(config, baseUrl+"/repos/"+user.Login+"/"+input+"/commits")
-	commit := []responseCommit{}
-	err = json.Unmarshal(repo, &commit)
+	fmt.Println("Masukkan Tahun :")
+	fmt.Scanf("%d", &year)
+	fmt.Println("Masukkan Bulan :")
+	fmt.Scanf("%d", &month)
+	fmt.Println("Masukkan Tanggal :")
+	fmt.Scanf("%d", &day)
+	parse := time.Date(year, month, day, 24, 00, 00, 0, time.UTC).Format("2022-01-12T00:00:00Z")
+	fmt.Println(parse)
+	repo := requestApiGithub(config, baseUrl+"/repos/"+user.Login+"/"+input+"/commits?since="+parse)
+
+	err = json.Unmarshal(repo, &resCommit)
 	PanicIfError(err)
-	fmt.Println(commit)
+	for _, res := range resCommit {
+		fmt.Println(res.Commit.Message)
+	}
 }
 
 func requestApiGithub(config Config, url string) []byte {
@@ -48,6 +66,7 @@ func requestApiGithub(config Config, url string) []byte {
 	}
 	req.Header.Add("Authorization", "token "+config.AccessToken)
 	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Accept", "vnd.github.v3+json")
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		panic(err)
