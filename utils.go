e9525728b7fe60c1dc9e7a3c6075aa0ba9fe57ab
MAC <mac@MACs-Mac-mini.lan>
2022-01-14 11:24:23 +0700

    add helper IsFileExist

--- utils.go
+++ utils.go
@@ -1,7 +1,19 @@
 package main
 
+import (
+	"os"
+)
+
 func PanicIfError(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func IsFileExist(filename string) bool {
+	fileInfo, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !fileInfo.IsDir()
 }
